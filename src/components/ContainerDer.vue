df48d8f8da8864ea06f7e36dabac5583bca95d3f
DennisPerez97 <dennis.perez.97@hotmail.com>
2022-05-14 17:19:35 -0500

    Agregando el componente ContainerDer y implementandolo en la vista HomeView

---
+++ src/components/ContainerDer.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="infocontainer">
+    <!--Contenedor completo -->
+    <div class="containerinfo1">
+      <!---Contenedor de la informacion izq, primer recuadro-->
+      <!--<br><br><br><br><br><br>-->
+      <h1>Fight Virus<br />With Vaccines<br /></h1>
+
+      <p>
+        Let's stop this pandemic by killing the virus<br />
+        with a vaccine, don't let yourself and your<br />
+        family get infected
+      </p>
+    </div>
+    <!--Termina contenedor infocontainer-->
+
+    <div class="imgcontainer-enfermera">
+      <!--Contenedor de la img de enfermera-->
+    </div>
+    <!--Termina contenedor img enfermera-->
+  </div>
+  <!--Termina infocontainer-->
+</template>
+
+<script>
+export default {
+  name: "ContainerDer",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: rgb(31, 62, 114);
+}
+
+.infocontainer {
+  /*Contenedor que ocupa todo el ancho de pantalla para poner info y a la enfermera*/
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  align-items: center;
+  background-color: aqua;
+}
+.containerinfo1 {
+  /*Contenedor de info*/
+  position: relative;
+  align-items: center;
+  float: left;
+  height: 65.9vw;
+  width: 51%;
+  overflow: hidden;
+  background-color: rgb(255, 0, 0);
+  padding: 9.5% 8.5%;
+}
+.containerinfo1 .icon1 {
+  width: 3%;
+}
+
+.imgcontainer-enfermera {
+  /*Contenedor de enfermera*/
+  position: relative;
+  float: right;
+  width: 49%;
+  height: 65.9vw;
+  overflow: hidden;
+  background-color: rgb(111, 0, 255);
+  transform: translate(0%, 0%);
+}
+.imgcontainer-enfermera img {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <ContainerDer msg="Welcome to Your Vue.js App" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import ContainerDer from "../components/ContainerDer.vue";
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+    ContainerDer,
   },
 };
 </script>
