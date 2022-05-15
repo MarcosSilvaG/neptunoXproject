ef1a1ed601e396bb14388efe7582366dcdf4e1ac
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-05-15 03:43:15 -0500

    Agregando css básico para la navbar

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,22 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <i class="bx bxl-meta"></i>
   </nav>
   <router-view />
 </template>
 
 <style>
+:root {
+  --text-color: #3232;
+  --text-color-alt: #5dadee2;
+  --bg-color: #d0d3d4;
+}
+
 * {
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -19,16 +27,12 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+ul {
+  list-style: none;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+a {
+  text-decoration: none;
+  color: initial;
 }
 </style>
